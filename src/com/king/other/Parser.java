18fe041723c4ddf5355a62ec3d6ce959de447d99
king <y1817629585@163.com>
2017-06-08 02:37:45 +0800

    基本完成了parser类，做了sql类的部分定义。

--- src/com/king/other/Parser.java
+++ src/com/king/other/Parser.java
@@ -18,7 +18,7 @@ public class Parser {
      * @return 返回Map，key为成员变量名，value为成员变量对应的值。
      * 注意：值为空的成员变量，map中的value也为空。
      */
-    public static <P> Map getFields(P pojo) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    public static <P> Map getFields(P pojo) throws Exception {
         Map map = new HashMap();
         Class cls = pojo.getClass();
         //获取模型类中的成员变量。
@@ -45,7 +45,6 @@ public class Parser {
      *
      * @param cls 目标表类的Class对象。
      * @return 目标类的成员变量集合。
-     *
      */
 
     public static Set getFieldName(Class cls) {
@@ -63,15 +62,24 @@ public class Parser {
 
     /**
      * 注入值到目标对象的成语变量中。
-     * @param cls 要注入的对象
-     *@param map 包含成员对象的名称和值的map对象。
-     * */
-    public static<P> P setFields(Class cls,Map map) throws IllegalAccessException, InstantiationException {
-        P pojo= ((P) cls.newInstance());
-        for (int i = 0; i < map.size(); i++) {
-
+     *
+     * @param cls 要注入的对象。
+     * @param map 包含成员对象的名称和值的map对象。
+     */
+    public static <P> P setFields(Class cls, Map map) throws Exception {
+        //实例化目标对象。
+        P pojo = ((P) cls.newInstance());
+        //获取key数组。
+        Object[] objects = map.keySet().toArray();
+        //遍历map对象。
+        for (int i = 0; i < objects.length; i++) {
+            //获取目标对象的指定成员变量。
+            java.lang.reflect.Field field = cls.getDeclaredField(objects[i].toString());
+            //获取成员变量指定的成员变量的set方法。
+            java.lang.reflect.Method method = cls.getDeclaredMethod(getMethodName(objects[i].toString(),1), field.getType());
+            //执行目标方法，将对应的注入到目标对象的成员变量中。
+            method.invoke(pojo, map.get(objects[i]));
         }
-
         return pojo;
     }
 
@@ -83,9 +91,9 @@ public class Parser {
      * @return 返回成员变量的值。
      * getMethodName(String name):合成目标成员变量的get方法名称。
      */
-    private static <P> Object getValue(String name, P pojo) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    private static <P> Object getValue(String name, P pojo) throws Exception {
         Class cls = pojo.getClass();
-        java.lang.reflect.Method method = cls.getMethod(getMethodName(name));
+        java.lang.reflect.Method method = cls.getMethod(getMethodName(name, 0));
         Object object = method.invoke(pojo);
         return object;
     }
@@ -93,12 +101,22 @@ public class Parser {
     /**
      * 合成一个成员变量的get方法。
      *
-     * @param name 成员变量的名称。
+     * @param name     成员变量的名称。
+     * @param getORset 指定取得get或者set方法。0:get方法;1: set方法。
      * @return 成员变量的get方法名。
      */
-    private static String getMethodName(String name) {
-        String methodName = "get" + name.substring(0, 1).toUpperCase() + name.substring(1);
-        return methodName;
+    private static String getMethodName(String name, int getORset) throws Exception {
+        String methodName = "";
+        switch (getORset) {
+            case 0:
+                methodName = "get" + name.substring(0, 1).toUpperCase() + name.substring(1);
+                return methodName;
+            case 1:
+                methodName = "set" + name.substring(0, 1).toUpperCase() + name.substring(1);
+                return methodName;
+            default:
+                throw new java.lang.Exception("!!!!!!!!!!!");
+        }
     }
 
 
---
+++ src/com/king/sql/Sql.java
@@ -0,0 +1,34 @@
+package com.king.sql;
+
+/**
+ * Created by yking on 17-6-8.
+ * SQL语句的合成
+ */
+public class Sql {
+    /**
+     *合成insect语句
+     *
+     */
+    public String getInsect(){
+        return null;
+    }
+    /**
+     * 合成Update语句
+     * */
+    public String getUpdate(){
+        return null;
+    }
+    /**
+     * 合成Delete语句
+     * */
+     public String getDelete(){
+        return null;
+    }
+    /**
+     * 合成Select语句
+     *
+     * */
+    public String getSelect(){
+        return null;
+    }
+}
